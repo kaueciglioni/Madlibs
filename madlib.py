4645acf9a4d35b34d767e3eb8cbe468612da9f15
Kaue Negrini Ciglioni <kaueciglioni@hotmail.com>
2024-05-07 15:59:15 -0300

    Criação de madlibs para interação com usuário e recrutadores no GitHub.

---
+++ madlib.py
@@ -0,0 +1,12 @@
+nome = input("Me informa seu nome:")
+idade = int(input("Quantos anos você tem?"))
+linguagem = input("Qual linguagem está consultando?")
+
+madlibs = f"Olá Kauê, meu nome é {nome}, eu tenho {idade} anos de idade! Estou analiando seu perfil, \
+    e encontrei esse madlib e estou conhecendo e visualizando os seus programas e as coisas que você é\
+          capaz de fazer com {linguagem}."
+print(madlibs)
+print(f"olá {nome}, seja bem vindo ao meu git hub, esse código é um dos meus diversos progrmamas em python\
+       em meu portifólio pra lhe apresentar as maravilhas do mundo python, que sou capaz de realizar! Muito\
+       obrogado pela sua atenção, caso queria conversar comigo, entre contato Numero: 11973412894 ou por e-mail:\
+       kaueciglioni@hotmail.com")
